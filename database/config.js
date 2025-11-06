e513dd13e2d3fb9dbd1b9671c9bba0d59b407aba
Gibshosting <gibrandwisatrio1305@gmail.com>
2025-11-07 00:46:34 +0700

    Add configuration for database connection

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: "7460490899:AAHdazKO4Ce0O8CITLDLi1Thi4qzXV6bt3Q", // token tetap kamu isi sendiri
+  owner: "6207641651",     // ID owner Telegram kamu
+  port: process.env.PORT || 3000 // untuk Railway
+  // konfigurasi lain tetap disimpan
+};
