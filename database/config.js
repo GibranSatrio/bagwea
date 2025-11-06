983200cae97c57eb625a2db7fc0a55787312692f
Gibshosting <gibrandwisatrio1305@gmail.com>
2025-11-07 00:54:11 +0700

    Change token key from 'token' to 'tokens'

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "7460490899:AAHdazKO4Ce0O8CITLDLi1Thi4qzXV6bt3Q", // token tetap kamu isi sendiri
+  tokens: "7460490899:AAHdazKO4Ce0O8CITLDLi1Thi4qzXV6bt3Q", // token tetap kamu isi sendiri
   owner: "6207641651",     // ID owner Telegram kamu
   port: process.env.PORT || 3000 // untuk Railway
   // konfigurasi lain tetap disimpan
